6a738a0623e3cfc78ba2e599b1cf656b09273747
MetaMaxfield <hardmen.mail@gmail.com>
2024-01-28 18:12:07 +0400

    Update translation.py, removing group localizations django-modeltranslation

--- users/translation.py
+++ users/translation.py
@@ -1,15 +1,8 @@
 from modeltranslation.translator import register, TranslationOptions
 from .models import CustomUser
-from django.contrib.auth.models import Group
 
 
 @register(CustomUser)
 class CustomUserTranslationOptions(TranslationOptions):
     """Мультиязычность выбранных полей"""
     fields = ('description',)
-
-
-@register(Group)
-class GroupTranslationOptions(TranslationOptions):
-    """Мультиязычность выбранных полей"""
-    fields = ('name',)

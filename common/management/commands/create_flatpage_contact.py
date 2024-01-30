b51c3f0cd822257ae40fe7f8f3362adabcd48d60
MetaMaxfield <hardmen.mail@gmail.com>
2024-01-30 13:55:22 +0400

    Add management\commands\create_flatpage_contact.py, custom django-admin command

---
+++ common/management/commands/create_flatpage_contact.py
@@ -0,0 +1,12 @@
+from django.contrib.flatpages.models import FlatPage
+from django.core.management import BaseCommand
+from flatpage_contact.models import NewFlatpage
+
+
+class Command(BaseCommand):
+    help = 'Создает плоскую страницу "О нас" с расширенными возможностями'
+
+    def handle(self, *args, **options):
+        about_flatpage = FlatPage.objects.create(url='/contact1/', title='О нас1', template_name='pages/contact1.html')
+        NewFlatpage.objects.create(flatpage=about_flatpage)
+        print('Плоская страница "О нас" с расширенными возможностями создана')

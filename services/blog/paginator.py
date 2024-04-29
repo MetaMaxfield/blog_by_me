40cb02a630bdbc37e6c21778979d491dfd7abc99
MetaMaxfield <hardmen.mail@gmail.com>
2024-04-29 14:09:07 +0400

    refactor: добавление параметра COUNT_POSTS_ON_PAGE
    Для улучшения читаемости и поддерживаемости кода добавлен параметр COUNT_POSTS_ON_PAGE, определяющий количество постов на страницу.
    
    - В файле settings.py добавлен параметр COUNT_POSTS_ON_PAGE
    - В файле paginator.py использован новый параметр COUNT_POSTS_ON_PAGE

--- services/blog/paginator.py
+++ services/blog/paginator.py
@@ -1,13 +1,14 @@
 from django.core.paginator import Paginator
 from django.db.models import QuerySet
 from django.http import HttpRequest
+from blog_by_me.settings import COUNT_POSTS_ON_PAGE
 
 
 def create_pagination(
         request: HttpRequest,
         object_list: QuerySet
 ) -> tuple[Paginator, QuerySet]:
-    paginator = Paginator(object_list, 3)
+    paginator = Paginator(object_list, COUNT_POSTS_ON_PAGE)
     page = request.GET.get('page')
     object_list = paginator.get_page(page)
     return paginator, object_list

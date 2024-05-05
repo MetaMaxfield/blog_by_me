1810c5e1924b4d362bdc0cc054486191fb2bdc48
MetaMaxfield <hardmen.mail@gmail.com>
2024-05-05 14:07:52 +0400

    refactor: использование генеративного представления для AuthorsView
    В файле users/views.py для улучшения поддерживаемости кода использование класса AuthorsView(View) заменено на альтернативный класс AuthorsView(ListView), который наследуется от генеративного представления ListView.
    
    - Добавлен класс AuthorsView(ListView)
    - Класс AuthorsView(View) закомментирован

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ import os
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
 from dotenv import load_dotenv
 from services.caching import get_cached_objects_or_queryset
 
@@ -9,13 +10,19 @@ from services.caching import get_cached_objects_or_queryset
 load_dotenv()
 
 
-class AuthorsView(View):
-    def get(
-            self,
-            request: HttpRequest
-    ) -> HttpResponse:
-        authors = get_cached_objects_or_queryset(os.getenv('KEY_AUTHORS_LIST'))
-        return render(request, 'users/author_list.html', {'authors': authors})
+# class AuthorsView(View):
+#     def get(
+#             self,
+#             request: HttpRequest
+#     ) -> HttpResponse:
+#         authors = get_cached_objects_or_queryset(os.getenv('KEY_AUTHORS_LIST'))
+#         return render(request, 'users/author_list.html', {'authors': authors})
+
+
+class AuthorsView(ListView):
+    queryset = get_cached_objects_or_queryset(os.getenv('KEY_AUTHORS_LIST'))
+    context_object_name = 'authors'
+    template_name = 'users/author_list.html'
 
 
 class AuthorDetailView(View):

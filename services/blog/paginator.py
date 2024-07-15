d067626ae7f8061b81aca92bdbf6f4ea1dbbf2c7
MetaMaxfield <hardmen.mail@gmail.com>
2024-07-15 06:49:42 +0400

    refactor: добавление аннотации типов для create_pagination()
    Добавлена аннотация типов к функции create_pagination() для улучшения читаемости и поддерживаемости кода.
    
    - Добавлена аннотация типов для функции create_pagination() в services/blog/paginator.py
    
    Связано с коммитом: cff01667

--- services/blog/paginator.py
+++ services/blog/paginator.py
@@ -1,7 +1,12 @@
 from django.core.paginator import Paginator
+from django.db.models import QuerySet
+from django.http import HttpRequest
 
 
-def create_pagination(request, object_list):
+def create_pagination(
+        request: HttpRequest,
+        object_list: QuerySet
+) -> tuple[Paginator, QuerySet]:
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     object_list = paginator.get_page(page)

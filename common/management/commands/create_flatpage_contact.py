e9828454ea3103832ebd49bee97919140ada362b
MetaMaxfield <hardmen.mail@gmail.com>
2024-11-25 18:53:24 +0400

    chore: улучшения команды create_flapage_contact
    В файле пользовательской команды create_flapage_contact.py добавлены улучшения для повышения информативности, обработки ошибок и удобства отслеживания выполнения команды.
    
    - Добавлена проверка на существование объекта NewFlatpage (модель для расширения модели плоской страницы, сделанная только для плоской страницы "О нас"). При его наличии выводится сообщение об ошибке, предотвращающее дублирование страницы
    - Добавлено логирование ошибки с уровнем ERROR для случаев, когда модель для расширения модели плоской страницы уже существует
    - Обновлен вывод сообщений для пользователей. Вместо print() используется stdout.write() с применением стилизованного вывода для ошибок и успешных действий

--- common/management/commands/create_flatpage_contact.py
+++ common/management/commands/create_flatpage_contact.py
@@ -1,13 +1,23 @@
+import logging
+
 from django.contrib.flatpages.models import FlatPage
 from django.core.management import BaseCommand
 
 from flatpage_contact.models import NewFlatpage
 
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
-    help = 'Создает плоскую страницу "О нас" с расширенными возможностями'
+    help = 'Создает плоскую страницу "О нас" с расширенными возможностями.'
 
     def handle(self, *args, **options):
-        about_flatpage = FlatPage.objects.create(url='/contact/', title='О нас', template_name='pages/contact.html')
-        NewFlatpage.objects.create(flatpage=about_flatpage)
-        print('Плоская страница "О нас" с расширенными возможностями создана')
+        if NewFlatpage.objects.exists():
+            info = 'Плоская страница "О нас" с расширенными возможностями уже существует.'
+            logger.error(info)
+            self.stdout.write(self.style.ERROR(info))
+        else:
+            info = 'Плоская страница "О нас" с расширенными возможностями создана.'
+            about_flatpage = FlatPage.objects.create(url='/contact/', title='О нас', template_name='pages/contact.html')
+            NewFlatpage.objects.create(flatpage=about_flatpage)
+            self.stdout.write(self.style.SUCCESS(info))

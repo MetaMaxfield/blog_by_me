25f93547313c14f5e055f559b1f9d72492d37168
MetaMaxfield <hardmen.mail@gmail.com>
2024-11-27 19:53:27 +0400

    refactor: использование константы для проверки оценки
    Обновлена логика в функции добавления рейтинга для обеспечения централизованного управления значением номинации.
    
    - В services/rating.py в функции create_or_update_rating() заменено прямое обращение к номинации "Лайк" в методе get() модели Mark на использование константы RU_TITLE_LIKE_MARK из файла settings.py

--- services/rating.py
+++ services/rating.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest
 from blog.models import Mark, Rating
 from services.caching import get_cached_objects_or_queryset
 from services.client_ip import get_client_ip
-from users.models import CustomUser
+from blog_by_me.settings import RU_TITLE_LIKE_MARK
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -21,7 +21,7 @@ def create_or_update_rating(request: HttpRequest) -> None:
         post=get_cached_objects_or_queryset(os.getenv('KEY_POST_DETAIL'), request.POST.get('post')),
         defaults={'mark_id': int(request.POST.get("mark"))},
     )
-    if int(request.POST.get('mark')) == Mark.objects.get(nomination='Лайк').id:
+    if int(request.POST.get('mark')) == Mark.objects.get(nomination_ru=RU_TITLE_LIKE_MARK).id:
         user = get_cached_objects_or_queryset(os.getenv('KEY_AUTHOR_DETAIL'), request.POST.get('author'))
         user.total_likes = F('total_likes') + 1
         user.save()

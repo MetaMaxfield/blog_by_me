92149702c0f762d10a00769354c1d54ad3ce514f
MetaMaxfield <hardmen.mail@gmail.com>
2025-03-30 13:41:47 +0400

    test: модель Video
    Добавлены тесты для модели Video с проверкой логики, определённой в коде проекта.
    
    - В blog/test_models.py пакета с тестами добавлен класс VideoModelTest(TestCase), содержащий:
        - setUpTestData() и setUp() для подготовки тестовых данных и оптимизации выполнения тестов
        - test_title_max_lenght() для проверки максимальной длины поля 'title'
        - test_title_verbose_name() для проверки метки поля 'title'
        - test_description_verbose_name() для проверки метки поля 'description'
        - test_file_upload_to() для проверки пути загрузки файлов для поля 'file'
        - test_file_validators() для проверки валидаторов, применяемых к полю 'file'
        - test_file_verbose_name() для проверки метки поля 'file'
        - test_create_at_auto_now_add() для проверки автоматического добавления времени создания в поле 'create_at'
        - test_object_name_is_title() для проверки метода __str__() на основе поля 'title'
        - test_model_ordering() для проверки порядка сортировки объектов модели
        - test_model_verbose_name() для проверки 'verbose_name' модели
        - test_model_verbose_name_plural() для проверки 'verbose_name_plural' модели

--- tests/blog/test_models.py
+++ tests/blog/test_models.py
@@ -1,6 +1,8 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.validators import FileExtensionValidator
 from django.test import TestCase
 
-from blog.models import Category
+from blog.models import Category, Video
 
 
 class CategoryModelTest(TestCase):
@@ -45,3 +47,63 @@ class CategoryModelTest(TestCase):
     def test_model_verbose_name_plural(self):
         fact_verbose_name_plural = self.category._meta.verbose_name_plural
         self.assertEqual(fact_verbose_name_plural, 'Категории')
+
+
+class VideoModelTest(TestCase):
+    """Тестирование модели Video"""
+
+    @classmethod
+    def setUpTestData(cls):
+        Video.objects.create(
+            title='Котята',
+            description='Забавные котята',
+            file=SimpleUploadedFile("cats.mp4", b"fake video content", content_type="video/mp4"),
+        )
+
+    @classmethod
+    def setUp(cls):
+        cls.video = Video.objects.get(title='Котята')
+
+    def test_title_max_lenght(self):
+        fact_max_length = self.video._meta.get_field('title').max_length
+        self.assertEqual(fact_max_length, 100)
+
+    def test_title_verbose_name(self):
+        fact_verbose_name = self.video._meta.get_field('title').verbose_name
+        self.assertEqual(fact_verbose_name, 'Заголовок видео')
+
+    def test_description_verbose_name(self):
+        fact_verbose_name = self.video._meta.get_field('description').verbose_name
+        self.assertEqual(fact_verbose_name, 'Описание видео')
+
+    def test_file_upload_to(self):
+        fact_upload_to = self.video._meta.get_field('file').upload_to
+        self.assertEqual(fact_upload_to, 'video/')
+
+    def test_file_validators(self):
+        fact_validators = self.video._meta.get_field('file').validators
+        self.assertEqual(fact_validators, [FileExtensionValidator(allowed_extensions=['mp4'])])
+
+    def test_file_verbose_name(self):
+        fact_verbose_name = self.video._meta.get_field('file').verbose_name
+        self.assertEqual(fact_verbose_name, 'Видеофайл')
+
+    def test_create_at_auto_now_add(self):
+        fact_auto_now_add = self.video._meta.get_field('create_at').auto_now_add
+        self.assertTrue(fact_auto_now_add)
+
+    def test_object_name_is_title(self):
+        expected_object_name = self.video.title
+        self.assertEqual(str(self.video), expected_object_name)
+
+    def test_model_ordering(self):
+        fact_ordering = self.video._meta.ordering
+        self.assertEqual(fact_ordering, ('-create_at',))
+
+    def test_model_verbose_name(self):
+        fact_verbose_name = self.video._meta.verbose_name
+        self.assertEqual(fact_verbose_name, 'Видеозапись')
+
+    def test_model_verbose_name_plural(self):
+        fact_verbose_name_plural = self.video._meta.verbose_name_plural
+        self.assertEqual(fact_verbose_name_plural, 'Видеозаписи')
